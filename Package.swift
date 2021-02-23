fe6e7799eb75dcea5ad44e1e79273b596ff1a2e2
Jaeyun,Oh <goodocy@linecorp.com>
2021-02-23 20:21:49 +0900

    Change minimum iOS version in SPM ( 13 -> 12 )

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "NavigationStack",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v12),
         .macOS(.v10_15)
     ],
     products: [
